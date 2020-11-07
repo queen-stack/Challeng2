85b31cfaf05ef8deb7ea5ad11dd72433e2bdadd4
JQueen <queen_jenf@yahoo.com>
2020-11-06 22:41:38 -0800

    commit on master

---
+++ test/sample-answers.js
@@ -0,0 +1,46 @@
+module.exports = {
+    name: 'Jenifer Queen',
+    github: 'https://github.com/queen-stack',
+    confirmAbout: true,
+    about:
+      'Dynamic leader with deep experience providing strategic expertise for application solutions, with experience driving core business strategies in corporate environments. Recognized for improving operational,  technical efficiencies, and aligning business objectives by implementing strategic plans, organizational structure, and process streamlining. Thrives in high-volume environments and can navigate evolving business needs, improve client experiences, and drive profitability. Easily understands technical concepts and implications, manage trade-offs and evaluate new opportunities with stakeholders.',
+          projects: [
+      {
+        name: 'smollTalk',
+        description:
+          'As communities scale, guarantee your users a place to belong by empowering them to start conversations that matter initially driven with specific topics',
+        languages: ['JavaScript', 'Express', 'Sequelize', 'Handlebars', 'Bootstrap', 'CSS', 'Dotenv' , 'bcrypt'],
+        link: 'https://github.com/queen-stack/smollTalk',
+        feature: true,
+        confirmAddProject: true
+      },
+      {
+        name: 'word web',
+        description:
+          'Dictionary app that allows user to search for a term returns definition, pronunciation and a new word of the day.  Provides more learning than bargained for! For those that are avid learners and looking to listen the word pronounced correctly.',
+        languages: ['JavaScript', 'HTML', 'CSS', 'API', 'Bootstrap'],
+        link: 'https://queen-stack.github.io/our_app_project/',
+        feature: true,
+        confirmAddProject: true
+      },
+      {
+        name: 'Taskmaster Pro',
+        description:
+          'Task Master Kanban application to help keep your project or life organized!',
+        languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+        link: 'https://queen-stack.github.io/taskmaster_pro/',
+        feature: false,
+        confirmAddProject: true
+      },
+      {
+        name: '5 Day Weather Forecast',
+        description:
+          'One of the first applications built learning how to use API calls',
+        languages: ['JavaScript' , 'server side API Calls'],
+        link: 'https://queen-stack.github.io/server_side_api/',
+        feature: false,
+        confirmAddProject: false
+      }
+    ]
+  };
+  
